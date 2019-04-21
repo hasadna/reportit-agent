c4cffcf9224564fd4f7db3656707356e959bed40
Noam <noamoss@gmail.com>
2019-04-21 15:10:24 +0300

    add basic function/structure to check the relevant NGOs per case

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { ScriptRunnerService, ContentService, FileUploader } from 'hatool';
 import { HubspotService } from './hubspot.service';
 import { switchMap } from 'rxjs/operators';
-
+import Script from '../assets/script.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['./app.component.less']
 })
+
 export class AppComponent implements OnInit {
   title = 'hatool';
 
   constructor(private runner: ScriptRunnerService,
               private content: ContentService,
-              private hubspot: HubspotService) {}
+              private hubspot: HubspotService,
+            ) {}
+
   ngOnInit() {
     this.content.sendButtonText = '';
     this.content.uploadFileText = 'לחצ/י לבחירת קובץ';
@@ -41,9 +44,27 @@ export class AppComponent implements OnInit {
               'assets/script.json',
               1,
               {
-                getOffender: async (record) => {
-                  return record.offender;
+                checkngos: async (record) => {
+                  const ngos = Script['3'].script.slice(1); // read the script with the list of NGOs, exclude the `default` thread
+                  const relevantngos = ngos.filter( (ngo) => {
+                      const ngoMetaData = ngo.script[0].meta; // get the ngo metadata
+                                        // run over the ngo relevancy conditions
+                      for (let condition = 0; condition <= ngoMetaData.length - 1; condition++) {
+                          const [conditionKey, conditionValue] = [ngoMetaData[condition].key, ngoMetaData[condition].value];
+
+                          if (            // here is the logic to decide which NGO is relevant. We can add any kind of logic.
+                              (conditionKey === 'offender' && conditionValue === record.offender) ||
+                              (conditionKey === 'complaintType' && conditionValue === record.complaint_type)
+                            )  {
+                            return 1;
+                        }
+                      }
+                    return 0;
+                    });
+                  console.log(`Relevant Ngos:`, relevantngos);
+                  return relevantngos;
                 },
+
                 combinedPoliceEventDescription: async (record) => {
                   return `${record.event_description} \n \n \
                                 פרטים נוספים, בשיחה עם המוקדנ/ית: ${record._police_more_details}, \n
@@ -86,4 +107,3 @@ export class AppComponent implements OnInit {
         ).subscribe(() => { console.log('done!'); });
   }
 }
-
