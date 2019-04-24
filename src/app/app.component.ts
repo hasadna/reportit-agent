80dff543757393c0c48d415e8a2550492169858a
Noam <noamoss@gmail.com>
2019-04-24 03:15:35 +0300

    complete functions to find relevant ngos/gov depts + choose who to share data with

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,6 +36,74 @@ export class AppComponent implements OnInit {
         return result;
       };
 
+
+      const retrieveRelevantOrgs = async (record, scriptNo) => {
+        // retrieve the the list of NGOs, exclude the `default` thread
+        const allOrgs: {'topic'?: string, 'script'?: any}[] = Script[scriptNo].script.slice(1);
+        const relevantorgs = allOrgs.filter( (org) => {
+              const orgMetaData = org.script[0].meta; // get the ngo metadata
+
+              // run over the organziation relevancy parameters
+              for (let condition = 0; condition <= orgMetaData.length - 1; condition++) {
+                const [conditionKey, conditionValue] = [orgMetaData[condition].key, orgMetaData[condition].value];
+
+                  if (            // here is the logic to decide which NGO is relevant. We can add any kind of logic.
+                      (conditionKey === 'offender' && conditionValue === record.offender) ||
+                      (conditionKey === 'complaintType' && conditionValue === record.complaint_type)
+                      ) { return 1; }
+                    }
+                  return 0;
+                });
+        return relevantorgs;
+      };
+
+      const selectOrgsToShareDataWith = async (record, scriptNo: string) => {
+        // function to ask user who to share the data with
+
+        const organizationsList = record._relevantorgs;
+
+        if (!record.send_contact_details_to_user) {record.send_contact_details_to_user = ''; }
+
+        let share_with_orgs = ' ';
+        // retrieve the repeating question + options to check per organization
+            const [question, options] = [Script[scriptNo].script[0]['script'][0].text,
+                                         Script[scriptNo].script[0]['script'][0].quick_replies.map(
+                                                              (x) => {
+                                                                        return {
+                                                                               'display' : x['title'],
+                                                                               'value' :  x['payload']
+                                                                               };
+                                                                      }
+                                                                )
+                                                      ];
+          console.log(`Relevant Orgs:`, organizationsList);
+
+          this.content.addTo(`נמצאו ${organizationsList.length} אפשרויות רלוונטיות לנושא הפניה:`);
+
+      // ask the returning questio for eaach relevant organization
+        for (const organization of organizationsList) {
+            try {
+              const orgName = organization.topic;
+              const orgDescription = organization.script[0].text;
+              // show the organziation details
+              this.content.addTo(`${orgName}: <br> ${orgDescription}`);
+
+              this.content.addOptions(question, options);
+              const shareWithThisOrg =  await this.content.waitForInput();
+
+              if (shareWithThisOrg === 'true') {                // if user approves, add org to the record field
+                share_with_orgs += `${orgName}, `;
+              } else if (shareWithThisOrg === 'askedForContactDetails') { // if user asked for the organization contact details
+                record.send_contact_details_to_user += `${orgName}, `;        // add the org name directly to the relevant record field
+              }
+            } catch (error) {
+              console.log('error ' + error);
+            }
+          }
+        console.log('share_with_org: ', share_with_orgs);
+        return await share_with_orgs;
+      } ;
+
     const vid = window.location.search.slice(1).split('&')[0].split('=')[1];
     this.hubspot.getUser(vid)
         .pipe(
@@ -47,62 +115,12 @@ export class AppComponent implements OnInit {
                 getOffender: async (record) => {
                   return record.offender;
                 },
-                chooseNGOsToShareWith: async (record) => {
-                  // retrieve the repeating question + options to check per organization
-                  const [question, options] = [Script['3'].script[0]['script'][0].text,
-                                               Script['3'].script[0]['script'][0].quick_replies.map(
-                                                      (x) => { return {
-                                                             'display' : x['title'],
-                                                             'value' :  x['payload']
-                                                          };
-                                                        })
-                                              ];
-                  console.log(`Relevant Ngos:`, record._relevantorgs);
+                checkRelevantNGOs: async (record) => retrieveRelevantOrgs(record, '3'),
+                chooseNGOsToShareWith: async (record) =>  selectOrgsToShareDataWith(record, '3'),
 
-                  this.content.addTo(`נמצאו ${record._relevantorgs.length} ארגונים:`);
-                  const org_to_share_with = [];
-                  const getData = async () => {
-                    for (const organization of record._relevantorgs) {
-                        try {
-                          const orgName = organization.topic;
-                          const orgDescription = organization.script[0].text;
-                          // show the organziation details
-                          this.content.addTo(`${orgName}: <br> ${orgDescription}`);
+                checkRelevantGovernmentDepartments: async (record) => retrieveRelevantOrgs(record, '4'),
+                chooseGovernmentDeptToShareWith: async (record) =>  selectOrgsToShareDataWith(record, '4'),
 
-                          this.content.addOptions(question, options);
-                          const share_with_org =  await this.content.waitForInput();
-
-                          if (share_with_org === true) {                // if user approves, add org to the record field
-                            org_to_share_with.push(orgName);
-                          }
-                        } catch (error) {
-                          console.log('error ' + error);
-                        }
-                        console.log('share_with_org: ', record.share_data_with_orgs);
-                  }
-                      return await share_with_org; } ;
-                  return getData();
-                },
-                checkRelevantNGOs: async (record) => {
-                  // retrieve the the list of NGOs, exclude the `default` thread
-                  const allOrgs = Script['3'].script.slice(1);
-                  console.log('all orgs:', allOrgs);
-                  const relevantorgs = allOrgs.filter( (org) => {
-                        const orgMetaData = org.script[0].meta; // get the ngo metadata
-
-                        // run over the organziation relevancy parameters
-                        for (let condition = 0; condition <= orgMetaData.length - 1; condition++) {
-                          const [conditionKey, conditionValue] = [orgMetaData[condition].key, orgMetaData[condition].value];
-
-                            if (            // here is the logic to decide which NGO is relevant. We can add any kind of logic.
-                                (conditionKey === 'offender' && conditionValue === record.offender) ||
-                                (conditionKey === 'complaintType' && conditionValue === record.complaint_type)
-                                ) { return 1; }
-                              }
-                            return 0;
-                          });
-                  return relevantorgs;
-                },
                 combinedPoliceEventDescription: async (record) => {
                   return `${record.event_description} \n \n \
                                 פרטים נוספים, בשיחה עם המוקדנ/ית: ${record._police_more_details}, \n
