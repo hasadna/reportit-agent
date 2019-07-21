be39be645ab62b57193ad62926dbb350e3634f2d
Adam Kariv <adam.kariv@gmail.com>
2019-07-21 17:40:38 +0300

    Fix org card layout and content

--- src/app/org-card/org-card.component.ts
+++ src/app/org-card/org-card.component.ts
@@ -12,17 +12,102 @@ export class OrgCardComponent implements OnInit {
   @Input() card: any;
   inner = {};
 
+  section(prefixes, label, format) {
+    let ret = '';
+    if (Array.isArray(prefixes)) {
+      for (let i = 1; i < 5; i++) {
+        const values = prefixes.map((p) => this.card[p + i]);
+        if (values.reduce((a, b) => a && b, true)) {
+          const formatted = format(values);
+          ret += `- ${formatted}\n`;
+        }
+      }
+    } else {
+      const value = this.card[prefixes];
+      if (value) {
+        ret = format(value) + '\n';
+      }
+    }
+    if (ret && label) {
+      ret = label + ':\n' + ret;
+    }
+    return ret;
+  }
+
+  email() {
+    return this.section(
+      ['Email '],
+      'כתובת מייל',
+      (x) => `[${x[0]}](mailto:${x[0]})`
+    );
+  }
+
+  website() {
+    return this.section(
+      ['website label ', 'website url '],
+      'קישורים',
+      (x) => `[${x[0]}](${x[1]})`
+    );
+  }
+
+  phone() {
+    return this.section(
+      ['Phone number '],
+      'קשר טלפוני',
+      (x) => `[${x[0]}](tel:${x[1]})`
+    );
+  }
+
+  fax() {
+    return this.section(
+      'Fax',
+      'פקס',
+      (x) => x
+    );
+  }
+
+
+  street_address() {
+    return this.section(
+      'Mail Address',
+      'כתובת משלוח דואר',
+      (x) => x
+    );
+  }
+
+  phone_response_details() {
+    return this.section(
+      'Phone response details',
+      null,
+      (x) => x
+    );
+  }
+
+  contacts() {
+    return this.section(
+      ['Contact Person '],
+      'אנשי קשר',
+      (x) => x
+    );
+
+  }
+
   ngOnInit() {
     const c = this.card;
+    console.log('CARD', c);
     this.inner = {
       title: c['Organization Name'],
       content: `
 ${c['Description']}
 
-כתובת מייל: ${c['Email 1']} ${c['Email 2']}
-
-[${c['website label 1']}](${c['webiste url 1']})
-      `
+${this.email()}
+${this.website()}
+${this.contacts()}
+${this.phone()}
+${this.phone_response_details()}
+${this.fax()}
+${this.street_address()}
+`
     };
   }
 
